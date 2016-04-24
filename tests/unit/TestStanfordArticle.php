8819b80e06dfc2d3239f178e69441ff5303ddcba
Witek Socha <witek.soch@gmail.com>
2016-04-24 19:56:21 +0200

    Add Stanford Article class + tests

---
+++ tests/unit/TestStanfordArticle.php
@@ -0,0 +1,36 @@
+<?php
+
+
+use DailyPhilosophy\Content\StanfordArticle;
+use \PHPHtmlParser\Dom;
+
+class StanfordArticleTest extends PHPUnit_Framework_TestCase
+{
+  public function testParser()
+  {
+    $dom = new Dom();
+    $this->assertInstanceOf('\PHPHtmlParser\Dom', $dom);
+    return $dom;
+  }
+
+  /**
+   * @depends testParser
+   */
+  public function testCreation(Dom $dom)
+  {
+    $stanArticle = new StanfordArticle($dom);
+    $this->assertObjectHasAttribute('domParser', $stanArticle);
+  }
+
+  /**
+   * @depends testParser
+   */
+  public function testDownloading(Dom $dom)
+  {
+    $stanArticle = new StanfordArticle($dom);
+    $stanArticle->downloadArticle();
+    $this->assertInternalType('string', $stanArticle->getIntro());
+  }
+
+
+}
