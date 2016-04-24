9f117e647fac1548462563f14e469e2ef433ddce
Witek Socha <witek.soch@gmail.com>
2016-04-24 19:52:27 +0200

    Add service provider to bootstrap

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,17 +2,29 @@
 require_once '../vendor/autoload.php';
 
 use DailyPhilosophy\MyApp;
+use DailyPhilosophy\Controllers\DiscoverController;
 
 $app = new MyApp();
 
-$app['htmlParser'] = $app->share(function() {
-  return new \PHPHtmlParser\Dom;
-});
-
-$app['debug'] = true;
+$app->register(new Silex\Provider\ServiceControllerServiceProvider());
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
   'twig.path' => __DIR__ . '/../resources/views'
 ));
+
+
+$app['htmlParser'] = function() {
+  return new \PHPHtmlParser\Dom;
+};
+
+
+$app['discover.controller'] = $app->share(function() use($app) {
+  return new DiscoverController($app['htmlParser']);
+});
+
+
+$app['debug'] = true;
+
+
 
 return $app;
