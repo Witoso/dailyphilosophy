a36bcd4216526481f8e2f456f9e93ca5bd7c33e6
Witek Socha <witek.soch@gmail.com>
2016-04-25 20:08:12 +0200

    Update home controller tests

--- tests/functional/HomeControllerTest.php
+++ tests/functional/HomeControllerTest.php
@@ -7,15 +7,15 @@ class HomeControllerTest extends WebTestCase
 {
     public function createApplication()
     {
-        return require dirname(__FILE__, 3) .'/app/bootstrap.php';
+        return require dirname(__FILE__, 3) .'/public/index.php';
     }
 
     public function testHomePage()
     {
         $client = $this->createClient();
         $crawler = $client->request('GET', '/');
-
         $this->assertTrue($client->getResponse()->isSuccessful());
-
+        $this->assertCount(1, $crawler->filter('h1:contains("dailyphilosophy")'));
+        $this->assertCount(4, $crawler->filter('a'));
     }
 }
