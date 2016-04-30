8ea1f3756b88c64ee4d7e3f69de65d13df917e6c
Witek Socha <witek.soch@gmail.com>
2016-04-30 12:27:40 +0200

    Add css, js, and edit views

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+$(() => {
+    $('.details').click((event) => {
+        console.log('hello');
+        let name = $(event.target).text()
+        $.ajax({
+            url: '/meet/' + name,
+            dataType: 'json',
+            error: () => {console.log('error occurred')},
+            success: (json) => {
+                console.log(json);
+                $('#name').text(name);
+                $('#image').attr('src', json.imageUrl);
+                $('#info').text(json.information);
+                $('#moreInfo').attr('href', json.detailsUrl);
+                $('#popup').css('display', 'block');
+            }
+        });
+    });
+
+
+    $('.close').click(() => {
+        $('#popup').css("display", "none");
+    });
+});
